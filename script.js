8c74766ff55081aa826f57696885ca5fef0e5387
Jonas <92853498+joraka@users.noreply.github.com>
2025-04-14 18:47:54 +0300

    countdown

--- script.js
+++ script.js
@@ -6,15 +6,55 @@ const numberButtonsEl = document.getElementById("numberButtons");
 const operatorsButtonsEl = document.getElementById("operatorButtons");
 const currentValueResultEl = document.getElementById("currentValueResult");
 const messageEl = document.getElementById("message");
+const timeEl = document.getElementById("time");
 
 let currentNums = "";
 let currentExpressionArray = [];
 let evaluatedResult = 0;
 let targetNum = undefined;
+let currentScore = 0;
 const disabledNumbers = [];
+const maxTimeAllowed = 15;
+let timeLeft = maxTimeAllowed;
+let currentInterval;
+
+function tryStartCountdown() {
+  if (currentInterval) return;
+  timeLeft = maxTimeAllowed;
+  timeEl.innerText = timeLeft;
+  currentInterval = setInterval(() => {
+    timeLeft -= 1;
+    if (timeLeft <= 5) {
+      timeEl.innerHTML = `<span style="color: red">${timeLeft}</span>`;
+      if (timeLeft < 1) {
+        gameOver();
+      }
+    } else {
+      timeEl.innerText = timeLeft;
+    }
+  }, 1000);
+}
+
+function stopCountdown() {
+  if (currentInterval) {
+    clearInterval(currentInterval);
+  }
+}
+
+function disableOperatorButtons(bool) {
+  for (const btn of operatorsButtonsEl.children) {
+    btn.disabled = bool;
+  }
+}
+
+function gameOver() {
+  stopCountdown();
+  setTimeout(() => {
+    alert("gameover");
+  }, 1000);
+}
 
 function calculateResult() {
-  console.log({ currentExpressionArray, currentNums });
   //display expression
   expressionEl.innerText = `${currentExpressionArray.join(" ")} ${currentNums}`;
 
@@ -30,55 +70,75 @@ function calculateResult() {
 function submitResult() {
   if (targetNum === evaluatedResult) {
     alert("corrent");
-    restartGame();
+    currentScore += 2;
   } else {
     alert("not correct");
-    restartGame();
+    currentScore -= 1;
   }
+  resetButtons();
+  scoreEl.innerText = currentScore;
 }
 
 //initialize numbers
 for (let i = 0; i < 10; i++) {
   numberButtonsEl.insertAdjacentHTML("beforeend", `<button value="${i}">${i}</button>`);
-  numberButtonsEl.children[i].addEventListener("click", (event) => {
-    const num = event.target.value;
-    currentNums += num;
-    disabledNumbers.push(event.target);
-    event.target.disabled = true;
-    calculateResult();
-  });
+  //on bu
+  numberButtonsEl.children[i].addEventListener("click", onNumberButtonClicked);
+}
+
+function onNumberButtonClicked(event) {
+  const num = event.target.value;
+  currentNums += num;
+  disabledNumbers.push(event.target);
+  event.target.disabled = true;
+  calculateResult();
+  disableOperatorButtons(false);
+  tryStartCountdown();
 }
 
 //on expression button clicked
 for (const btn of operatorsButtonsEl.children) {
   btn.addEventListener("click", (event) => {
-    // if(typeof currentExpressionArray.at(-1) === 'string') return;
     if (currentNums.length > 0) {
       currentExpressionArray.push(Number(currentNums), event.target.value);
       currentNums = "";
+      calculateResult();
+      disableOperatorButtons(true);
+      clearDisabledNumberButtons();
+    } else {
       calculateResult();
     }
   });
 }
 
 //submit button clicked
-submitBtn.addEventListener("click", (event) => {
+submitBtn.addEventListener("click", () => {
   submitResult();
 });
 
-//game restart logic
-function restartGame() {
+function resetButtons() {
   targetNum = Math.floor(Math.random() * 101);
   targetEl.innerText = targetNum;
-
-  //clear disabled buttons
-  disabledNumbers.forEach((btn) => (btn.disabled = false));
-  disabledNumbers.length = 0;
 
   currentValueResultEl.innerText = "";
   expressionEl.innerText = "";
   currentNums = "";
   currentExpressionArray.length = 0;
+
+  disableOperatorButtons(true);
+  clearDisabledNumberButtons();
+}
+
+function clearDisabledNumberButtons() {
+  //clear disabled buttons
+  disabledNumbers.forEach((btn) => (btn.disabled = false));
+  disabledNumbers.length = 0;
+}
+
+//game restart logic
+function restartGame() {
+  resetButtons();
+  scoreEl.innerText = currentScore;
 }
 
 restartGame();
